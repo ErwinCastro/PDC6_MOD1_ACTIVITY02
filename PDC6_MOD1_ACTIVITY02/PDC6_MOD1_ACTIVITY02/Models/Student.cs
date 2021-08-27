0d585d41788daf18ad68154a6c3065110d0a9916
Erwin Castro <castro.erwinjohn@auf.edu.ph>
2021-08-27 21:37:40 +0800

    Add project files.

---
+++ PDC6_MOD1_ACTIVITY02/PDC6_MOD1_ACTIVITY02/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC6_MOD1_ACTIVITY02.Models
+{
+    class Student
+    {
+        public string StudentId { get; set; }
+        public string Name { get; set; }
+
+        public string CourseCode { get; set; }
+        public string CourseTitle { get; set; }
+        public int Noofunits { get; set; }
+        public int Date { get; set; }
+    }
+}
---
+++ PDC6_MOD1_ACTIVITY02/PDC6_MOD1_ACTIVITY02/ViewModels/SubjectViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PDC6_MOD1_ACTIVITY02.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PDC6_MOD1_ACTIVITY02.ViewModels
+{
+    class SubjectViewModel
+    {
+       public Student Info { get; set; }
+        public SubjectViewModel()
+        {
+            Info = new Student
+            {
+                StudentId = "12",
+                Name = "Erwin John Castro",
+                CourseCode = "Cap02",
+                CourseTitle = "Capstone 02 Graduation",
+                Noofunits = 5,
+                Date = 011020,
+
+
+
+            };
+        }
+
+    }
+}
